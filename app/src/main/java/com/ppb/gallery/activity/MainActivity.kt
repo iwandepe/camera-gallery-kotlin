fa007eb5f671b01db01e7e640f4b17c0ee73de47
cahindo <erikfaderik@gmail.com>
2021-11-15 20:38:06 +0700

    feat: save original file, fix layout, prepare upload

--- app/src/main/java/com/ppb/gallery/activity/MainActivity.kt
+++ app/src/main/java/com/ppb/gallery/activity/MainActivity.kt
@@ -4,15 +4,19 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ppb.gallery.R
@@ -21,17 +25,37 @@ import com.ppb.gallery.adapter.GalleryImageClickListener
 import com.ppb.gallery.adapter.Image
 import com.ppb.gallery.fragment.GalleryFullscreenFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+import android.graphics.BitmapFactory
+import android.view.View
+import android.webkit.MimeTypeMap
+import android.widget.ImageButton
+
 
 class MainActivity : AppCompatActivity(), GalleryImageClickListener {
     // gallery column count
     private val SPAN_COUNT = 3
-    private val CAMERA_REQUEST = 1888
+//    private val CAMERA_REQUEST = 1888
     private val imageList = ArrayList<Image>()
-    lateinit var galleryAdapter: GalleryImageAdapter
+    private lateinit var galleryAdapter: GalleryImageAdapter
+    private lateinit var currentPhotoPath: String
+    val REQUEST_IMAGE_CAPTURE = 1
+    lateinit var ivTest : ImageView
+    lateinit var btnUpload : ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ivTest = findViewById(R.id.ivTest) as ImageView
+        btnUpload = findViewById(R.id.btnUpload) as ImageButton
+        btnUpload.setOnClickListener {
+            uploadImage()
+        }
+
         // init adapter
         galleryAdapter = GalleryImageAdapter(imageList)
         galleryAdapter.listener = this
@@ -45,7 +69,7 @@ class MainActivity : AppCompatActivity(), GalleryImageClickListener {
 
         if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
                 == PackageManager.PERMISSION_DENIED)
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_IMAGE_CAPTURE)
 
         setSupportActionBar(toolbar)
     }
@@ -61,8 +85,10 @@ class MainActivity : AppCompatActivity(), GalleryImageClickListener {
             R.id.action_add -> {
                 Toast.makeText(applicationContext, "click on add image", Toast.LENGTH_LONG).show()
 
-                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivityForResult(cameraIntent, CAMERA_REQUEST)
+//                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE)
+
+                dispatchTakePictureIntent()
 
                 return true
             }
@@ -86,16 +112,18 @@ class MainActivity : AppCompatActivity(), GalleryImageClickListener {
         imageList.add(Image("https://i.ibb.co/dBCHzXQ/paris.jpg", "Paris Eiffel"))
         imageList.add(Image("https://i.ibb.co/JKB0KPk/pizza.jpg", "Pizza Time"))
         imageList.add(Image("https://i.ibb.co/VYYPZGk/salmon.jpg", "Salmon "))
-        imageList.add(Image("https://i.ibb.co/JvWpzYC/sunset.jpg", "Sunset in Beach"))
+//        imageList.add(Image("https://i.ibb.co/JvWpzYC/sunset.jpg", "Sunset in Beach"))
+        imageList.add(Image("http://i.picsum.photos/id/54/1600/1000.jpg?hmac=VEsMDh66FUrSTzsWaVs6UJsWtGspZcZi5pu2k1sqBZ4", "Iris ITS"))
         galleryAdapter.notifyDataSetChanged()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CAMERA_REQUEST) {
-            val photo: Bitmap = data?.extras?.get("data") as Bitmap
-            val ivTest = findViewById(R.id.ivTest) as ImageView
-            ivTest.setImageBitmap(photo)
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+//            val photo: Bitmap = data?.extras?.get("data") as Bitmap
+//            ivTest.setImageBitmap(photo)
+
+            setPic()
 
             // TODO: Captured image quality is very low
             // TODO: Capture image still use extra
@@ -113,4 +141,88 @@ class MainActivity : AppCompatActivity(), GalleryImageClickListener {
         galleryFragment.setArguments(bundle)
         galleryFragment.show(fragmentTransaction, "gallery")
     }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+            Log.d("Main", "Photopath : "+currentPhotoPath)
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    Log.e("main", "dispatchTakePictureIntent IOException: ")
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        photoFile
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+    private fun galleryAddPic() {
+        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            val f = File(currentPhotoPath)
+            mediaScanIntent.data = Uri.fromFile(f)
+            sendBroadcast(mediaScanIntent)
+        }
+    }
+
+    private fun setPic() {
+        // Get the dimensions of the View
+        val targetW: Int = ivTest.getWidth()
+        val targetH: Int = ivTest.getHeight()
+
+        // Get the dimensions of the bitmap
+        val bmOptions = BitmapFactory.Options()
+        bmOptions.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions)
+        val photoW = bmOptions.outWidth
+        val photoH = bmOptions.outHeight
+
+        // Determine how much to scale down the image
+        val scaleFactor = Math.max(1, Math.min(photoW / targetW, photoH / targetH))
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false
+        bmOptions.inSampleSize = scaleFactor
+        bmOptions.inPurgeable = true
+        val bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions)
+        ivTest.setImageBitmap(bitmap)
+    }
+
+    private fun uploadImage(){
+        Toast.makeText(applicationContext, "Upload image", Toast.LENGTH_SHORT).show()
+
+
+
+    }
+//    https://stackoverflow.com/questions/20322528/uploading-images-to-server-android
+//    https://handyopinion.com/upload-file-to-server-in-android-kotlin/
+//    https://www.youtube.com/watch?v=dbSAIuyHInY
+
 }
